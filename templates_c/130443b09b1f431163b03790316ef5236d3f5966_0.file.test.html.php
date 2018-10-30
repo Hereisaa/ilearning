d7fbd6370bfc6681fe1862b554a399475b5513d2
bbjack1qoo1 <bbjack1qoo1@gmail.com>
2018-10-30 17:46:08 +0800

    Update templates_c/130443b09b1f431163b03790316ef5236d3f5966_0.file.test.html.php

---
+++ templates_c/130443b09b1f431163b03790316ef5236d3f5966_0.file.test.html.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-10-30 09:18:12
+  from 'D:\xampp\htdocs\awei_ilearning\ilearning\templates\test.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5bd813c4de49c5_87475372',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '130443b09b1f431163b03790316ef5236d3f5966' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\awei_ilearning\\ilearning\\templates\\test.html',
+      1 => 1540540425,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bd813c4de49c5_87475372 (Smarty_Internal_Template $_smarty_tpl) {
+?><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>測驗</title>
+</head>
+<body>
+    <p>測驗頁面</p>
+</body>
+</html><?php }
+}
